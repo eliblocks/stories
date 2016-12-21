7308021e722275bd0da82658763e128dc302e2db
eli block <eliblock@yahoo.com>
2016-12-21 11:57:56 -0800

    create users from auth hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,14 @@ class SessionsController < ApplicationController
 
   def create
     @auth = request.env['omniauth.auth']
-    process(@auth)
+    if User.find_by(email: @auth.info.email)
+      @user = User.find_by(email: @auth.info.email)
+    else
+      @user = User.new
+      @user.process(@auth)
+      @user.save
+    end
+    debugger
     redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+
+  def process(auth)
+    self.email = auth.info.email
+    self.name = auth.info.name
+    self.image = auth.info.image
+    self.verified = auth.info.verified
+    self.facebook_id = auth.extra.raw_info.id
+    self.first_name = auth.extra.raw_info.first_name
+    self.last_name = auth.extra.raw_info.last_name
+    self.link = auth.extra.raw_info.link
+    self.gender = auth.extra.raw_info.gender
+    self.picture = auth.extra.raw_info.picture.data.url
+    self.timezone = auth.extra.raw_info.timezone
+    self.updated_time = auth.extra.raw_info.updated_time
+    self.locale = auth.extra.raw_info.locale
+    #need to test other age ranges
+    self.age_range = auth.extra.raw_info.age_range.min.join
+  end
 end
