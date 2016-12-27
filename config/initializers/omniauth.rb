2527e93539c216115aeb1c2fb23b1bebea653276
eli block <eliblock@yahoo.com>
2016-12-27 13:19:53 -0800

    configurations

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,8 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['APP_ID'], ENV['APP_SECRET'],
+  scope: 'email,public_profile',
+  info_fields: 'id,cover,name,first_name,last_name,age_range,link,gender,locale,picture,timezone,updated_time,verified,email',
+  image_size: 'large'
+end
+
+
