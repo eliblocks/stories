f1f619afd4ba2636b676eb60c02a18196ca36c88
eli block <eliblock@yahoo.com>
2017-02-10 13:30:23 -0800

    reverse test break

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   validates :facebook_id, uniqueness: true, allow_nil: true
 
   def self.followed_ids
-    Relationship.selec(:followed_id).distinct
+    Relationship.select(:followed_id).distinct
   end
 
   def self.writers
